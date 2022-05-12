83ed1bb8603d59c5a4fcc07982f1a21a90dd17fa
Giles Tetteh <giles@aims.edu.gh>
2022-05-12 02:48:31 +0200

    Update install.sh
    misc

--- install.sh
+++ install.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 sed -i "s%\$WORKDIR%$(pwd)%g" nextcloud.service
-sudo cp bonescreen.service /etc/systemd/system/
-sudo cp bonescreen.service /lib/systemd/system/
+sudo cp nextcloud.service /etc/systemd/system/
+sudo cp nextcloud.service /lib/systemd/system/
 
 chmod +x start.sh
 chmod +x stop.sh
